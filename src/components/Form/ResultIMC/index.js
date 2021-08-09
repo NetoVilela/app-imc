3221eed61cfdb1eea62d6a0abaf177278ff44c15
NetoVilela <favneto.a.v.n@gmail.com>
2021-08-09 20:58:01 -0300

    Centraliza resultado

--- src/components/Form/ResultIMC/index.js
+++ src/components/Form/ResultIMC/index.js
@@ -6,7 +6,7 @@ import styles from'./style'
 export default function ResultIMC(props){
     const {resultImc, messageResultIMC} = props;
     return(
-        <View>
+        <View style={styles.resultbox}>
             <Text style={styles.textResult}>{resultImc !=null ? messageResultIMC : ''}</Text>
             <Text style={styles.result}>{resultImc}</Text>
         </View>
--- src/components/Form/ResultIMC/style.js
+++ src/components/Form/ResultIMC/style.js
@@ -2,21 +2,18 @@ import {StyleSheet} from 'react-native'
 
 const styles = StyleSheet.create({
     textResult:{
-        alignItems: 'center',
-        justifyContent: 'center',
         color: "#FF0043",
         fontSize: 18,
         marginTop: 20,
-        marginLeft: 100
     },
     result:{
-        alignItems: 'center',
-        justifyContent: 'center',
-        fontSize: 25,
+        fontSize: 48,
         marginTop:10,
         fontWeight: "bold",
         color: "#FF0043",
-        marginLeft: 140
+    },
+    resultbox:{
+        alignItems: 'center',
     }
 });
 
